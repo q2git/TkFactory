fe731167c1adf6eb44b36e0463e2479c766c7f9f
q2git <54710@sohu.com>
2016-09-04 18:22:32 +0800

    Update example.py

--- example.py
+++ example.py
@@ -20,9 +20,13 @@ gui.widgets['mn1'].entryconfig(1,command=lambda: fun(1))
 gui.widgets['mn-3'].entryconfig(0,command=lambda: gui.textvariables['mnbt1'].set('HELLO'))    
 gui.widgets['s1'].config(command=lambda x=0:gui.textvariables['ls1'].set(gui.widgets['s1'].get()))
 gui.widgets['pg1'].start(50)
+def fun2(widget):
+    val= widget.item(widget.selection()[0],'values')
+    gui.widgets['cb1'].config(values=list(val))
+gui.widgets['tree1'].bind('<<TreeviewSelect>>',lambda x: fun2(gui.widgets['tree1']))
 for name in gui.textvariables.keys():
-    gui.textvariables[name].set(name)
-    gui.widgets['tree1'].insert('', 0, iid=name,text=name, 
+    #gui.textvariables[name].set(name)
+    gui.widgets['tree1'].insert('', 0, iid=name,text=name.upper(), 
         values=tuple(gui.widgets[name].keys())#('a','b',234,324,'c','你好',),
         )
     
